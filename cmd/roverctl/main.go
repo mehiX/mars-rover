0c480d98b2bef3262bd7da0805480fa80e13a0b9
Mihai Oprea <mihai.git@pm.me>
2021-05-14 13:40:37 +0200

    added command flags

--- cmd/roverctl/main.go
+++ cmd/roverctl/main.go
@@ -2,30 +2,47 @@ package main
 
 import (
 	"context"
+	"flag"
 	"fmt"
 
 	"github.com/mehiX/mars-rover/rover"
 )
 
-func main() {
-	fmt.Println("Mars rover")
+var (
+	name      string
+	x, y      int
+	direction string
+	command   string
+)
 
-	kata, err := rover.NewRover("Kata", 3, 5, "N")
+func init() {
+	flag.StringVar(&name, "n", "(no name)", "name of the rover")
+	flag.IntVar(&x, "x", 0, "start X coordinate")
+	flag.IntVar(&y, "y", 0, "start y coordinate")
+	flag.StringVar(&direction, "d", "N", "initial pointing direction")
+	flag.StringVar(&command, "c", "", "commands to send to the rover")
+
+	flag.Parse()
+}
+
+func main() {
+
+	rvr, err := rover.NewRover(name, x, y, direction)
 	if err != nil {
 		panic(err)
 	}
 
-	fmt.Printf("Start: %s\n", kata)
+	fmt.Printf("Start: %s\n", rvr)
 
 	ctx, cancel := context.WithCancel(context.Background())
 
-	ch := rover.Commands(ctx, "FBERLFBLLRFFFF", kata)
+	ch := rover.Commands(ctx, command, rvr)
 
 	for c := range ch {
 		c.Execute()
 	}
 
-	fmt.Printf("End: %s\n", kata)
+	fmt.Printf("End: %s\n", rvr)
 
 	cancel()
 }
